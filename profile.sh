fdc59847da4b128d2209e908a7f5c32bd7c5f46b
raki123 <rafael.kiesel@web.de>
2023-02-14 14:12:00 +0100

    Updated profiling script.

--- profile.sh
+++ profile.sh
@@ -1,3 +1,3 @@
-python -m cProfile -o output.pstats main.py "$@"
+python -m cProfile -o output.pstats aspmc/main.py "$@"
 gprof2dot -f pstats output.pstats | dot -Tpng -o output.png
 xdg-open output.png
